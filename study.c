34d2346893da901b352efa49254deea9fb6f0350
SilentMicro <138412737+SilentMicro@users.noreply.github.com>
2023-07-03 16:39:54 +0800

    Add files via upload

---
+++ study.c
@@ -0,0 +1,130 @@
+
+#include <stdio.h>
+
+int main (){
+  char a;
+ a=getchar();
+
+   if (a%2==1)
+      printf ("是奇数");
+    else
+      printf ("不是奇数"); 
+     
+return 0;
+}
+
+
+
+
+//int main(){
+//	int a=1;
+//	while (a<=10){
+//	
+//	
+//	printf("%d\n",a);
+//	
+//	a++;
+//	
+//	}
+//	
+//	
+//	return 0;
+//}
+//
+
+// main(){
+// 	int a=0;
+// 	
+// 	printf("%d\n",a);
+// 	
+// 	if (a%2==1)
+// 	    
+// 	    printf ("是奇数");
+// 	else 
+// 	    printf("不是奇数"); 
+// 	
+// 	
+// 	
+// 	
+// 	return 0; 
+// } 
+// 
+// 
+ 
+ 
+// int main () {
+//    int a = 10;
+// 	scanf ("%d\n",&a) ;
+// 	if (a<18)
+// 	  printf ("未成年\n"); 
+// 	else 
+// 	printf ("成年\n"); 
+// 	
+// 	
+// 	
+// 	return 0;
+// }
+// 
+// 
+ 
+ 
+ 
+//#define LENGTH 10   
+//#define WIDTH  5
+//#define NEWLINE '\n'
+// 
+//int main()
+//{
+// 
+//   int area;  
+//  
+//   area = LENGTH * WIDTH;
+//   printf("value of area : %d", area);
+//   printf("%c", NEWLINE);
+// 
+//   return 0;
+//}
+
+
+
+
+//#include <stdio.h>
+// 
+//// 函数外定义变量 x 和 y
+//int x;
+//int y;
+//int addtwonum()
+//{
+//    // 函数内声明变量 x 和 y 为外部变量
+//    extern int x;
+//    extern int y;
+//    // 给外部变量（全局变量）x 和 y 赋值
+//    x = 1;
+//    y = 2;
+//    return x+y;
+//}
+// 
+//int main()
+//{
+//    int result;
+//    // 调用函数 addtwonum
+//    result = addtwonum();
+//    
+//    printf("result 为: %d\a",result);
+//    
+//    return 0;
+
+
+//int main (){
+//  int A=10;
+//  int* P= &A;
+// 
+//  printf("%d\n",sizeof(P));
+// 
+//
+//
+//   return 0;
+//} 
+
+
+
