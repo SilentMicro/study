d265039354e67a2d04914d1954e918f120e1d471
SilentMicro <138412737+SilentMicro@users.noreply.github.com>
2023-07-04 20:51:11 +0800

    Add files via upload

---
+++ 2023-7-4.c
@@ -0,0 +1,238 @@
+#include <stdio.h>
+#include <string.h>
+#include <windows.h>
+#include <stdlib.h>
+
+
+
+
+
+//int main(){
+//	char A[]="violet_evergarden";
+//	char B[]="#################";
+//	int S=sizeof(A)/sizeof(A[0])-2;
+//	int L=0;
+//	int R=S;
+//	
+//	while(L<=R)
+//	{
+//		
+//	B[L]=A[L];
+//	B[R]=A[R];
+//	L++;
+//	R--;
+//	printf("%s\n",B);
+//	
+//	Sleep(1000);
+//	system("cls");
+//    }
+//	
+//	printf ("%s\n",B);
+//	return 0;
+//}
+
+
+
+//int main(){
+//	
+//	int A[10]={1,2,3,6,5,4,7,8,9,10};
+//   
+//	int i=0 ;
+//	int k;
+//	int s=sizeof(A)/sizeof(A[0]);
+//	
+//	scanf("%d",&k);
+//	while(i<=s-1)
+//	{  if(A[i]!=k)
+//	    {
+//	    	i++;
+//	    }
+//	    else
+//		{
+//			 printf("下标为%d",i);
+//			 break;
+//		}	    
+//    }
+//  
+//    if (i>s-1)	
+//	{
+//    	printf("不存在");
+//	}
+//	
+//  	return 0;
+//}
+//
+//
+
+
+
+//
+//int main(){
+//	
+//	int A[10]={1,2,3,4,5,6,7,8,9,10};
+//	int k=7;
+//	
+//	int s=sizeof(A)/sizeof(A[0]);
+//	int l=0;
+//	int r=s-1;
+//	
+//	
+//	while (l<=r)
+//	{
+//		
+//   	int m=(l+r)/2;
+//	
+//    	if(A[m]>k)
+//    	{
+//    	r=m-1;
+//         }
+//         
+//    	else if (A[m]<k)
+//	    {
+//        l=m+1;
+//     	}
+//     	
+//    	else 
+//		{
+//	  	printf ("下标为%d",m);
+//    	break;	
+//    	}
+//	}
+//	
+//	if (l>r)
+//	{
+//	printf("w");
+//    }
+//    
+//	return 0;
+//}
+
+
+
+
+//
+//int main (){
+//	
+//	char a[10]={0};
+//	char b[10]={0};
+//	char c;
+//	
+//	printf("请输入账号\n");
+//	scanf("%d",a);
+//	
+//	
+//	while(c=getchar()!='\n'){
+//		;
+//	}
+//	
+//	printf("请输入密码\n");
+//	scanf("%s",b);
+//	
+//	while(c=getchar()!='\n'){
+//		;
+//	}
+//	
+//	printf("%s\n",b);
+//	
+//
+//	if (b =='qweasd')
+//	{
+//	
+//	  printf("账号密码正确，请确认登录\nYES");
+//	  
+//     }
+//     else{
+//     	printf("账号或密码错误");
+//	 }
+//     
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+//
+//int main(){
+//	
+//	int a;
+//	int b;
+//	int c=1;
+//	int d=0;
+//	printf("n=");
+//	scanf("%d",&a);
+//	
+//    	for(b=1;b<=a;b++)
+//		{
+//	   	 c=c*b;
+//      	 printf("%d的阶乘为,%d\n",b,c);
+//	   
+//            d=d+c;
+//		}
+//    	 
+//	     printf ("以上的和为%d",d);
+//	return 0;
+//}
+//
+//
+//
+
+//int main(){
+//	
+//	char A[10]={0};
+//	
+//	char b ;
+//	char c;
+//	
+//	printf("输入密码");
+//	
+//	scanf("%s",A);
+//	
+//    while(c=getchar()!='\n') {
+//    	;
+//	}
+//
+//
+//	printf("请确认 Y/N\n");
+//
+//	b=getchar();
+//	
+//	if (b=='Y')
+//	{
+//	      printf("确认成功");}
+//	      
+//	else
+//	{
+//	
+//	      printf("放弃确认");
+//     }
+//    
+//
+//	return 0;
+//}
+//
+
+
+
+
+//int main(){
+//	
+//	int a=0 ;
+//	
+//	for(a=1;a<=10;a++)
+//	
+//     {   printf ("%d\n",a);
+//	 
+//	
+//	}
+//	
+//	
+//	
+//	return 0;
+//}
+
+
